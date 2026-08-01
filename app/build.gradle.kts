8c4af22bd0b86f5f25813221d0b13b5709d20cbe
Luyando Shilukukwa <48803829+Luyandosnt@users.noreply.github.com>
2026-08-01 12:23:06 +0200

    Release Ankyra 1.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.gecogames.ankyra"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.8.0"
+        versionCode = 10
+        versionName = "1.9.0"
     }
 
     buildFeatures {
