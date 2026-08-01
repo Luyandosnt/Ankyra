6c7a1324359494dfd777db10b15658b1e2a14f9b
Luyando Shilukukwa <48803829+Luyandosnt@users.noreply.github.com>
2026-08-01 10:09:59 +0200

    Release Ankyra 1.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.gecogames.ankyra"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.6.0"
+        versionCode = 8
+        versionName = "1.7.0"
     }
 
     buildFeatures {
